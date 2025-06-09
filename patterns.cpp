83bf110de6b0b96a0ca6e5f757224591f4521da4
Harsh <harshtyagi0008@icloud.com>
2025-06-09 21:40:13 +0530

    add patterns file

---
+++ patterns.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+using namespace std;
+void blockpattern(int a){
+    for(int i=0;i<a;i++){
+        for(int j=0;j<=a;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void increasingorderpattern(int b){
+    for(int i=0;i<b;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void increasingnumberpattern(int c){
+    for(int i=0;i<c;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+void decreasingnumberpattern(int d){
+    for(int i=d;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+void decreasingorderpattern(int e){
+    for(int i=e;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+void pyramidpattern(int f){
+    for(int i=1;i<=f;i++){
+        for(int space=1;space<=f-i;space++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void reversepyramidpattern(int g){
+    for(int i=g;i>=1;i--){
+        for(int space=1;space<=g-i;space++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void diamondpattern(int h){
+    for(int i=1;i<=h;i++){
+        for(int space=1;space<=h-i;space++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=h;i>=1;i--){
+        for(int space=1;space<=h-i;space++){
+            cout<<" ";
+        }
+        for(int j=1;j<=2*i-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void arrowpattern(int k){
+    for(int i=0;i<k;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=k;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+void binarypattern(int l){
+    int start=1;
+    for(int i=0;i<l;i++){
+        if(i%2==0){
+            start=1;
+        }
+        else{
+            start=0;
+        }
+        for(int j=0;j<i;j++){
+            cout<<start;
+            start=1-start;
+        }
+        cout<<endl;
+    }
+}
+void increasingcrownpattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        for(int space=1;space<=2*(n-i);space++){
+            cout<<" ";
+        }
+        for(int j=i;j>=1;j--){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void newnumberpattern(int n){
+    int num=1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<num;
+            num+=1;
+        }
+        cout<<endl;
+    }
+}
+void increasingCharpattern(int n){
+    for(int i=0;i<=n;i++){
+        for(char j='A';j<='A'+i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void decreasingCharpattern(int n){
+    for(int i=n;i>=0;i--){
+        for(char j='A';j<='A'+i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+void newCharpattern(int n){
+    for(int i=0;i<=n;i++){
+        char ch='A'+i;
+        for(int j=0;j<=i;j++){
+            cout<<ch;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int x;
+    cin>>x;
+    for(int i=0;i<x;i++){
+        int n;
+        cin>>n;
+        //blockpattern(n);
+        //increasingorderpattern(n);
+        //increasingnumberpattern(n);
+        //decreasingnumberpattern(n);
+        //decreasingorderpattern(n);
+        //pyramidpattern(n);
+        //reversepyramidpattern(n);
+        //diamondpattern(n);
+        //arrowpattern(n);
+        //binarypattern(n);
+        //increasingcrownpattern(n);
+        //newnumberpattern(n);
+        //increasingCharpattern(n);
+        //decreasingCharpattern(n);
+        newCharpattern(n);
+    }
+    return 0;
+}
