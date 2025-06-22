a07bf350726638a4ffd37064cc78766e44e83d53
Harsh <harshtyagi0008@icloud.com>
2025-06-22 14:47:12 +0530

    add array problems part 4

--- arrayproblems4.cpp
+++ arrayproblems4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 using namespace std;
 //Problem 1: Find the longest subarray with sum k
 void FindingSubArray(int n,int arr[],int k){
@@ -30,6 +31,18 @@ void FindingElements(int n,int arr[],int k){
     }
     cout << "The elements that add up to " << k << " are: " << a << " and " << b << endl;
 }
+// Problem 3: Find the indexes of the elements that add up to target value k
+void FindingIndexes(int n,int arr[],int k){
+    unordered_map<int, int> mp;
+    for (int i=0;i<n;i++){
+        int compliment = k - arr[i];
+        if(mp.find(k - arr[i]) != mp.end()){
+            cout << "The indexes of the elements that add up to " <<k<< " are: " <<mp[compliment]<< " and " <<i<<endl;
+            return;
+        }
+        mp[arr[i]]=i;
+    }
+}
 int main(){
     int size;
     cout << "Enter the size of the array: ";
@@ -44,6 +57,7 @@ int main(){
     cin >> k;
     //cout<<"The result is: ";
     //FindingSubArray(size, arr, k);
-    FindingElements(size, arr, k);
+    //FindingElements(size, arr, k);
+    FindingIndexes(size, arr, k);
     return 0;
 }
