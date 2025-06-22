5e1c2b325ab2bf54d73e559dbb382f377a0775d0
Harsh <harshtyagi0008@icloud.com>
2025-06-22 14:39:29 +0530

    solved another problem

--- arrayproblems4.cpp
+++ arrayproblems4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_set>
 using namespace std;
 //Problem 1: Find the longest subarray with sum k
 void FindingSubArray(int n,int arr[],int k){
@@ -15,6 +16,20 @@ void FindingSubArray(int n,int arr[],int k){
     }
     cout<<maxLength;
 }
+//Problem 2: Find if the array elements add up to target value k
+void FindingElements(int n,int arr[],int k){
+    unordered_set<int> s;
+    int a=0,b=0;
+    for(int i=0;i<n;i++){
+        if(s.find(k - arr[i]) != s.end()){
+            a = k - arr[i];
+            b = arr[i];
+            break;
+        }
+        s.insert(arr[i]);
+    }
+    cout << "The elements that add up to " << k << " are: " << a << " and " << b << endl;
+}
 int main(){
     int size;
     cout << "Enter the size of the array: ";
@@ -27,7 +42,8 @@ int main(){
     int k;
     cout << "Enter the sum k: ";
     cin >> k;
-    cout<<"The result is: ";
-    FindingSubArray(size, arr, k);
+    //cout<<"The result is: ";
+    //FindingSubArray(size, arr, k);
+    FindingElements(size, arr, k);
     return 0;
 }
