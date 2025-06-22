27a2b118df49dbfd9d8c818584c7582cc91897e7
Harsh <harshtyagi0008@icloud.com>
2025-06-22 19:40:39 +0530

    add array problems part 5

---
+++ arrayproblems5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+//Problem 1: Sort an array just containing 0s, 1s, and 2s
+void Sorting(int n,int arr[]){
+    //We gonna use the Dutch National Flag algorithm to sort the array, it states
+    //[0 to low-1] contains 0s extreme left
+    //[low to mid-1] contains 1s
+    //[mid to high] contains random elements (0s, 1s, 2s)
+    //[high+1 to n-1] contains 2s extreme right
+    int low=0,mid=0,high=n-1;
+    while(mid <= high){
+        if(arr[mid] == 0){
+            swap(arr[low], arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid] == 1){
+            mid++;
+        }
+        else{
+            swap(arr[mid], arr[high]);
+            high--;
+        }
+    }
+}
+int main(){
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    int arr[size];
+    cout << "Enter the elements of the array (0s, 1s, and 2s only): ";
+    for(int i = 0; i < size; i++){
+        cin >> arr[i];
+    }
+    Sorting(size, arr);
+    cout << "Sorted array: ";
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
