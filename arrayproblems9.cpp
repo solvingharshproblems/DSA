bbcba30baa11730522b786bf6a16effe0dbeb4f0
Harsh <harshtyagi0008@icloud.com>
2025-06-26 14:23:50 +0530

    solved problem manually

--- arrayproblems9.cpp
+++ arrayproblems9.cpp
@@ -5,6 +5,27 @@ using namespace std;
 void nextPermutation(int arr[], int n) {
     next_permutation(arr, arr + n);
 }
+// But if stl is not allowed, we can implement it manually
+void nextPermutationManual(int arr[], int n) {
+    int index=-1;
+    for(int i=n-2;i>=0;i--){
+        if(arr[i]<arr[i+1]){
+            index=i;
+            break; 
+        }
+    }
+    if(index==1){
+        reverse(arr, arr + n);
+        return;
+    }
+    for(int i=n-1;i>index;i--){
+        if(arr[i]>arr[index]){
+            swap(arr[i], arr[index]);
+            break;
+        }
+    } 
+    reverse(arr + index + 1, arr + n);
+}
 int main(){
     int size;
     cout << "Enter the size of the array: ";
@@ -14,7 +35,8 @@ int main(){
     for (int i = 0; i < size; i++) {
         cin >> arr[i];
     }
-    nextPermutation(arr, size);
+    //nextPermutation(arr, size);
+    nextPermutationManual(arr, size);
     cout << "Next permutation is: ";
     for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
