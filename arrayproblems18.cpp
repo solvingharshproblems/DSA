a4f9a791ad1f3128ae1ab0039f8fc45888b21474
Harsh <harshtyagi0008@icloud.com>
2025-08-16 22:30:22 +0530

    add array problems part 18, problem 2

--- arrayproblems18.cpp
+++ arrayproblems18.cpp
@@ -41,9 +41,119 @@ int MergeIntervalsOptimal(vector<vector<int>> &arr){
     return result.size();
 }
 //TC = O(nlogn+n)
+//Problem 2: Merge sorted arrays without any extra space 
+void MergeArrayBruteForce(long long arr1[],long long arr2[],int n,int m){
+    long long arr3[n+m];
+    int left=0,right=0,index=0;
+    while(left<n && right<m){
+        if(arr1[left]<=arr2[right]){
+            arr3[index]=arr1[left];
+            left++,index++;
+        }
+        else{
+            arr3[index]=arr2[right];
+            right++,index++;
+        }
+    }
+    while(left<n){
+        arr3[index++]=arr1[left++];
+    }
+    while(right<m){
+        arr3[index++]=arr2[right++];
+    }
+    for(int i=0;i<n+m;i++){
+        if(i<n){
+            arr1[i]=arr3[i];
+        }
+        else{
+            arr2[i-n]=arr3[i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr1[i]<<" ";
+    }
+    for(int j=0;j<m;j++){
+        cout<<arr2[j]<<" ";
+    }
+}
+void MergeArrayBetter(long long arr1[],long long arr2[],int n,int m){
+    int left=n-1,right=0;
+    while(left>0 && right<m){
+        if(arr1[left]>arr2[right]){
+            swap(arr1[left],arr2[right]);
+        }
+        else{
+            break;
+        }
+    }
+    sort(arr1,arr1+n);
+    sort(arr2,arr2+m);
+    for(int i=0;i<n;i++){
+        cout<<arr1[i]<<" ";
+    }
+    for(int j=0;j<m;j++){
+        cout<<arr2[j]<<" ";
+    }
+}
+void SwapIfGreater(long long arr1[],long long arr2[],int n,int m){
+    if(arr1[n]>arr2[m]){
+        swap(arr1[m],arr2[m]);
+    }
+}
+void MergeArrayOptimal(long long arr1[],long long arr2[],int n,int m){
+    int len=n+m;
+    int gap=(len/2)+(len%2);
+    while(gap>0){
+        int left=0;
+        int right=left+gap;
+        while(right<len){
+            if(left<n && right>=n){
+                SwapIfGreater(arr1,arr2,left,right-n);
+            }
+            else if(left>=n){
+                SwapIfGreater(arr2,arr2,left-n,right-n);
+            }
+            else{
+                SwapIfGreater(arr1,arr1,left,right);
+            }
+            left++,right++;
+        }
+        if(gap==1) break;
+        gap=(gap/2)+(gap%2);
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr1[i]<<" ";
+    }
+    for(int j=0;j<m;j++){
+        cout<<arr2[j]<<" ";
+    }
+}
 int main(){
+    /*
     vector<vector<int>> arr={{1,3},{2,6},{8,10},{15,18}};
     cout<<MergeIntervalBruteForce(arr)<<endl;
     cout<<MergeIntervalsOptimal(arr)<<endl;
+    */ 
+    int n,m;
+    cout<<"Enter size of 1st array: ";
+    cin>>n;
+    long long arr1[n];
+    cout<<"Enter the elements of 1st array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr1[i];
+    }
+    cout<<"Enter size of 2nd array: ";
+    cin>>m;
+    long long arr2[m];
+    cout<<"Enter the elements of 2nd array: ";
+    for(int i=0;i<m;i++){
+        cin>>arr2[i];
+    }
+    cout<<"Resultant array: "<<endl;
+    MergeArrayBruteForce(arr1,arr2,n,m);
+    cout<<endl;
+    MergeArrayBetter(arr1,arr2,n,m);
+    cout<<endl;
+    MergeArrayOptimal(arr1,arr2,n,m);
     return 0;
 }
