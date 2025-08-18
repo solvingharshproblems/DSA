639c84a2fa1b93cefc9f7731f58d6d2d14394be8
Harsh <harshtyagi0008@icloud.com>
2025-08-18 17:02:57 +0530

    add array problems part 20, problem 2

--- arrayproblems20.cpp
+++ arrayproblems20.cpp
@@ -64,6 +64,65 @@ int MergeSort(int arr[],int low,int high){ //Divide and Merge
 void FindingReversePairsOptimal(int n,int arr[]){
     cout<<"No. of pairs are: "<<MergeSort(arr,0,n-1)<<endl;
 } //TC = O(n logn + n logn) SC = O(n + n)
+//Problem 2: Find the maximum product subarray
+void SubarrayProductBruteForce(int n,int arr[]){
+    int maxx=INT_MIN;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int product=1;
+            for(int k=i;k<j;k++){
+                product*=arr[k];
+            }
+            maxx=max(product,maxx);
+        }
+    }
+    cout<<maxx<<endl;
+}
+void SubarrayProductBetter(int n,int arr[]){
+    int maxx=INT_MIN;
+    for(int i=0;i<n;i++){
+        int product=1;
+        for(int j=i;j<n;j++){
+            product*=arr[j];
+            maxx=max(product,maxx);
+        }
+    }
+    cout<<maxx<<endl;
+}
+//There are 2 optimal approaches of this code 1. Observations 2. Modified version of Kadane's algorithm
+//1. Observations & 2-pointer approach from starting and end 
+void SubarrayProductOptimal1(int n,int arr[]){
+    int maxx=INT_MIN;
+    int prefix=1,suffix=1;
+    for(int i=0;i<n-1;i++){
+        if(prefix==0){
+            prefix=1;
+        }
+        if(suffix==0){
+            suffix=1;
+        }
+        prefix=prefix*arr[i];
+        suffix=suffix*arr[n-i-1];
+        maxx=max(maxx,max(prefix,suffix));
+    }
+    cout<<maxx<<endl;
+}
+//2. Modified version of Kadane's algorithm
+void SubarrayProductOptimal2(int n,int arr[]){
+    int maxx=arr[0];
+    int minn=arr[0];
+    int result=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<0){
+            swap(maxx,minn);
+        }
+        maxx=max(arr[i],maxx*arr[i]);
+        minn=min(arr[i],minn*arr[i]);
+        result=max(result,maxx);
+    }
+    cout<<result;
+}
+//Note: Never show this approach to interviewers as this is not intuitive at all
 int main(){
     long long size;
     cout<<"Enter the size of an array: ";
@@ -73,7 +132,13 @@ int main(){
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
+    /*
     FindingReversePairsBruteForce(size,arr);
     FindingReversePairsOptimal(size,arr);
+    */
+    SubarrayProductBruteForce(size,arr);
+    SubarrayProductBetter(size,arr);
+    SubarrayProductOptimal1(size,arr);
+    SubarrayProductOptimal2(size,arr);
     return 0;
 }
