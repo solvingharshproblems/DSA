4ce82a70298202c04cbf6cd60b38b34bd275ef92
Harsh <harshtyagi0008@icloud.com>
2025-08-20 16:24:45 +0530

    add binary search 2, first n last occurence

--- binarysearch2.cpp
+++ binarysearch2.cpp
@@ -41,6 +41,50 @@ void FirstNLastOccurence(int n,int arr[],int key){
     cout<<"First occurence of "<<key<<" is at index: "<<lb<<endl;
     cout<<"Last occurence of "<<key<<" is at index: "<<UpperBound(n,arr,key)-1<<endl;
 } //TC = O(2*log n) 
+//If the interviewer asks for write this problem without using lower bound and upper bound functions,
+//We can solve this problem using two separate binary search functions for first and last occurence.
+void FirstOccurence(int n,int arr[],int key){
+    int low=0,mid,high=n-1;
+    int first=-1;
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]==key){
+            first=mid;
+            high=mid-1; 
+        }
+        else if(arr[mid]<key){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    if(first==-1){
+        cout<<"Theres no occurence of "<<key<<" in the array!"<<endl;
+        return;
+    }
+    else{
+        cout<<"First occurence of "<<key<<" is at index: "<<first<<endl;
+    }
+} //TC=O(log n)
+void LastOccurence(int n,int arr[],int key){
+    int low=0,mid,high=n-1;
+    int last=-1;
+    while(low<=high){
+        mid=(low+high)/2;
+        if(arr[mid]==key){
+            last=mid;
+            low=mid+1;
+        }
+        else if(arr[mid]<key){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    cout<<"Last occurence of "<<key<<" is at index: "<<last<<endl;
+} //TC=O(log n)
 int main(){
     int size;
     cout<<"Enter the size of array: ";
@@ -54,5 +98,7 @@ int main(){
     cout<<"Enter the key to search: ";
     cin>>key;
     FirstNLastOccurence(size,arr,key);
+    FirstOccurence(size,arr,key);
+    LastOccurence(size,arr,key);
     return 0;
 }
