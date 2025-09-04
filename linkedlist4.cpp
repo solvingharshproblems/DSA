30fe667be67506f22298dc3321d4f1d53d72df85
Harsh <harshtyagi0008@icloud.com>
2025-09-04 16:57:38 +0530

    add linked list 4, problem 2

--- linkedlist4.cpp
+++ linkedlist4.cpp
@@ -98,6 +98,56 @@ Node* Sort012Optimal(Node* head){
     delete twoDummy;
     return head;
 }
+//TC=O(n)
+//Problem 2: Remove nth node from the end of the linked list
+//For brute force approach we can keep a count of the number of nodes and join result=count-nth node from start to the node next to nth node from end
+Node* RemoveNthFromEndBruteForce(Node* head,int n){
+    if(head==nullptr){
+        return head;
+    }
+    int count=0;
+    Node* temp=head;
+    while(temp!=nullptr){
+        count++;
+        temp=temp->next;
+    }
+    if(n==count){
+        Node* newHead=head->next;
+        delete head;
+        return newHead;
+    }
+    int pos=count-n;
+    temp=head;
+    for(int i=1;i<pos;i++){
+        temp=temp->next;
+    }
+    Node* nodeToDelete=temp->next;
+    temp->next=temp->next->next;
+    delete nodeToDelete;
+    return head;
+} //TC=O(2n) 
+//For optimal approach we can use 2 pointers slow and fast and move fast pointer 2 steps ahead and then move both pointers one step at a time until fast pointer reaches the end
+//Now slow pointer will be at the node previous to the nth node from the end
+Node* RemoveNthFromEndOptimal(Node* head,int n){
+    if(head==nullptr){
+        return head;
+    }
+    Node* dummy=new Node(-1);
+    dummy->next=head;
+    Node* slow=dummy;
+    Node* fast=dummy;
+    for(int i=0;i<n;i++){
+        fast=fast->next;
+    }
+    while(fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    Node* nodeToDelete=slow->next;
+    slow->next=slow->next->next;
+    delete nodeToDelete;
+    return dummy->next;
+} //TC=O(n)
 int main(){
     int size;
     cout<<"Enter size of linked list: ";
@@ -107,6 +157,7 @@ int main(){
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
+    /*
     Node* head1=Sort012BruteForce(ConvertArray2LL(arr));
     cout<<"The elements of the sorted linked list are: ";
     Node* temp=head1;
@@ -122,5 +173,25 @@ int main(){
         cout<<temp->data<<" ";
         temp=temp->next;
     }
+    */
+   int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    Node* head3=RemoveNthFromEndBruteForce(ConvertArray2LL(arr),n);
+    cout<<"The elements of the modified linked list are: ";
+    Node* temp=head3;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    Node* head4=RemoveNthFromEndOptimal(ConvertArray2LL(arr),n);
+    cout<<"The elements of the modified linked list are: ";
+    temp=head4;
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
     return 0;
 }
