c4961f6fbca35f51609ed763c1752e1d0e180788
Harsh <harshtyagi0008@icloud.com>
2025-09-07 10:40:34 +0530

    add linked list 7, problem 2

--- linkedlist7.cpp
+++ linkedlist7.cpp
@@ -55,6 +55,48 @@ bool DetectLoopOptimal(Node* head){
     }
     return false;
 } //TC=O(n) 
+//Problem 2: Find the length of the loop in a LL
+//For Brute Force Approach we can use a hash map to store the visited nodes and when we find a visited node we can count the number of nodes in the loop
+int LengthOfLoopBruteForce(Node* head){
+    unordered_map<Node*,bool> visited;
+    Node* temp=head;
+    while(temp!=nullptr){
+        if(visited[temp]){
+            int count=1;
+            Node* loopNode=temp->next;
+            while(loopNode!=temp){
+                count++;
+                loopNode=loopNode->next;
+            }
+            return count;
+        }
+        visited[temp]=true;
+        temp=temp->next;
+    }
+    return 0;
+} //TC=O(n) SC=O(n)
+//For Optimal Approach we will use Tortoise and Hare Algorithm to detect the loop and then count the number of nodes in the loop
+int LengthOfLoopOptimal(Node* head){
+    if(head==nullptr || head->next==nullptr){
+        return 0;
+    }
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=nullptr && fast->next!=nullptr){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            int count=1;
+            Node* loopNode=slow->next;
+            while(loopNode!=slow){
+                count++;
+                loopNode=loopNode->next;
+            }
+            return count;
+        }
+    }
+    return 0;
+} //TC=O(n) SC=O(1)
 int main(){
     int size;
     cout<<"Enter size of linked list: ";
@@ -72,5 +114,7 @@ int main(){
     }else{
         cout<<"No loop detected in the linked list"<<endl;
     }
+    cout<<"Length of the loop in the linked list is: "<<LengthOfLoopBruteForce(head1)<<endl;
+    cout<<"Length of the loop in the linked list is: "<<LengthOfLoopOptimal(head1)<<endl;
     return 0;
 }
