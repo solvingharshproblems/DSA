c624717f06d56026c6d7e04be3530d0fb2453d93
Harsh <harshtyagi0008@icloud.com>
2025-09-11 16:23:33 +0530

    add linked list 11, problem 2

--- linkedlist11.cpp
+++ linkedlist11.cpp
@@ -45,7 +45,68 @@ Node* RotateLL(Node* head,int k){
     temp->next=nullptr;//breaking the circular LL
     return head;
 } //TC=O(2n)
+//Problem 2: Merge two sorted LL
+//For Brute Force Approach we can store elements of both LLs in an array,sort it and then convert it to LL
+Node* MergeLLsBruteForce(Node* head1,Node* head2){
+    if(head1==nullptr){
+        return head2;
+    }
+    if(head2==nullptr){
+        return head1;
+    }
+    vector<int> arr;
+    Node* temp=head1;
+    while(temp!=nullptr){
+        arr.push_back(temp->data);
+        temp=temp->next;
+    }
+    temp=head2;
+    while(temp!=nullptr){
+        arr.push_back(temp->data);
+        temp=temp->next;
+    }
+    sort(arr.begin(),arr.end());
+    return ConvertArray2LL(arr);
+} //TC=O((n+m)log(n+m)) SC=O(n+m)
+//For Optimal Approach we can use the concept of dummy node denoted by mergedHead
+Node* MergeLLsOptimal(Node* head1,Node* head2){
+    if(head1==nullptr){
+        return head2;
+    }
+    if(head2==nullptr){
+        return head1;
+    }
+    Node* mergedHead=nullptr;
+    if(head1->data<head2->data){
+        mergedHead=head1;
+        head1=head1->next;
+    }
+    else{
+        mergedHead=head2;
+        head2=head2->next;
+    }
+    Node* temp=mergedHead;
+    while(head1!=nullptr && head2!=nullptr){
+        if(head1->data<head2->data){
+            temp->next=head1;
+            head1=head1->next;
+        }
+        else{
+            temp->next=head2;
+            head2=head2->next;
+        }
+        temp=temp->next;
+    }
+    if(head1!=nullptr){
+        temp->next=head1;
+    }
+    if(head2!=nullptr){
+        temp->next=head2;
+    }
+    return mergedHead;
+} //TC=O(n+m)
 int main(){
+    /*
     int size;
     cout<<"Enter size of linked list: ";
     cin>>size;
@@ -60,6 +121,31 @@ int main(){
     cin>>k;
     cout<<"Linked List after rotation: ";
     Node* temp=RotateLL(head,k);
+    while(temp!=nullptr){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+    */
+    int size1,size2;
+    cout<<"Enter size of first linked list: ";
+    cin>>size1;
+    vector<int> arr1(size1);
+    cout<<"Enter elements of first linked list: ";
+    for(int i=0;i<size1;i++){
+        cin>>arr1[i];
+    }
+    Node* head1=ConvertArray2LL(arr1);
+    cout<<"Enter size of second linked list: ";
+    cin>>size2;
+    vector<int> arr2(size2);
+    cout<<"Enter elements of second linked list: ";
+    for(int i=0;i<size2;i++){
+        cin>>arr2[i];
+    }
+    Node* head2=ConvertArray2LL(arr2);
+    cout<<"Merged Linked List: ";
+    Node* temp=MergeLLsOptimal(head1,head2);
     while(temp!=nullptr){
         cout<<temp->data<<" ";
         temp=temp->next;
