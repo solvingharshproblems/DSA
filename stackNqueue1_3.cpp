399a102179cbdb8b6d29e15fea68da44e996060d
Harsh <harshtyagi0008@icloud.com>
2025-09-14 12:03:27 +0530

    add stack & queue part 1, queue implementation using stack

--- stackNqueue1_3.cpp
+++ stackNqueue1_3.cpp
@@ -49,6 +49,54 @@ class Stack{
             return size;
         } // TC=O(1) SC=O(1)
 };
+//Implementing queue using stack
+class Queue{
+    public:
+        stack<int> s1;
+        stack<int> s2;
+        int size=0;
+        int Enqueue(int x){
+            s1.push(x);
+            size++;
+            return x;
+        } // TC=O(1) SC=O(1)
+        int Dequeue(){
+            if(s1.size()==0 && s2.size()==0){
+                cout<<"Queue Underflow"<<endl;
+                return -1;
+            }
+            else{
+                if(s2.size()==0){
+                    while(s1.size()!=0){
+                        s2.push(s1.top());
+                        s1.pop();
+                    }
+                }
+                int x=s2.top();
+                s2.pop();
+                size--;
+                return x;
+            }
+        } // TC=O(n) SC=O(1)
+        int Front(){
+            if(s1.size()==0 && s2.size()==0){
+                cout<<"Queue is empty"<<endl;
+                return -1;
+            }
+            else{
+                if(s2.size()==0){
+                    while(s1.size()!=0){
+                        s2.push(s1.top());
+                        s1.pop();
+                    }           
+                }
+                return s2.top();
+            }
+        } // TC=O(n) SC=O(1)
+        int Size(){
+            return size;
+        } // TC=O(1) SC=O(1)
+};
 int main(){
     Stack s;
     s.Push(10);
@@ -58,5 +106,13 @@ int main(){
     cout<<"The size of the stack is: "<<s.Size()<<endl; 
     cout<<"Popped element is: "<<s.Pop()<<endl; 
     cout<<"The size of the stack is: "<<s.Size()<<endl;
+    Queue q;
+    q.Enqueue(10);
+    q.Enqueue(20);
+    q.Enqueue(30);
+    cout<<"The front element is: "<<q.Front()<<endl;
+    cout<<"The size of the queue is: "<<q.Size()<<endl; 
+    cout<<"Dequeued element is: "<<q.Dequeue()<<endl; 
+    cout<<"The size of the queue is: "<<q.Size()<<endl;
     return 0;
 }
