6560a338835bb1017bb827be6b3e791cd9c9917c
Harsh <harshtyagi0008@icloud.com>
2025-09-16 16:35:16 +0530

    add stack & queue part 3, problem 2

--- stackNqueue3.cpp
+++ stackNqueue3.cpp
@@ -24,6 +24,71 @@ int isBalanced(string s){
     }
     return st.empty();
 } //TC=O(n) SC=O(n)
+//Problem 2: Designing a min stack that supports getMin() in O(1) time and O(1) space
+//For Brute Force Approach we will declare a class stack and define a min function which will return the minimum element in the stack by traversing the entire stack.
+class MinStack{
+    stack<int> st;
+    public:
+    void push(int x){
+        st.push(x);
+    }
+    void pop(){
+        if(!st.empty()){
+            st.pop();
+        }
+    }
+    int top(){
+        if(!st.empty()){
+            return st.top();
+        }
+        return -1;
+    }
+    int getMin(){   
+        if(st.empty()){
+            return -1;
+        }
+        int minEle=st.top();
+        stack<int> temp=st;
+        while(!temp.empty()){
+            minEle=min(minEle,temp.top());
+            temp.pop();
+        }
+        return minEle;
+    }
+}; //TC=O(n) SC=O(n)
+//For Optimal Approach we will use an auxiliary stack to keep track of the minimum elements.
+class MinStackOptimal{
+    stack<int> st;
+    stack<int> minSt;
+    public:
+    void push(int x){
+        st.push(x);
+        if(minSt.empty() || x<=minSt.top()){
+            minSt.push(x);
+        }
+    }
+    void pop(){
+        if(!st.empty()){
+            if(st.top()==minSt.top()){
+                minSt.pop();
+            }
+            st.pop();
+        }
+    }
+    int top(){
+        if(!st.empty()){
+            return st.top();
+        }
+        return -1;
+    }
+    int getMin(){   
+        if(minSt.empty()){
+            return -1;
+        }
+        return minSt.top();
+    }
+}; //TC=O(1) SC=O(n) 
+
 int main(){
     string s;
     cout<<"Enter the expression: ";
@@ -34,5 +99,14 @@ int main(){
     else{
         cout<<"Not Balanced"<<endl;
     }
+    MinStackOptimal ms;
+    ms.push(3);
+    ms.push(5);
+    ms.push(2);
+    ms.push(1);
+    ms.push(9);
+    ms.push(7);
+    cout<<"Top element: "<<ms.top()<<endl; 
+    cout<<"Minimum element: "<<ms.getMin()<<endl;
     return 0;
 }
