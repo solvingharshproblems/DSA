bcbe51972cec9825211f25cd884243d6e8477f0b
Harsh <harshtyagi0008@icloud.com>
2025-09-17 11:54:08 +0530

    add stack & queue part 4, problem 2

--- stackNqueue4.cpp
+++ stackNqueue4.cpp
@@ -35,6 +35,41 @@ void nextGreaterElementOptimal(int arr[], int n){
         cout<<res[i]<<" ";
     }
 } //TC=O(2n) SC=O(n)
+//Problem 2: Next Greater Element in a Circular Array
+//For Brute Force Approach we will use two nested loops to find the next greater element for each element in the circular array.
+void nextGreaterElementCircularBruteForce(int arr[], int n){
+    for(int i=0;i<n;i++){
+        int next=-1;
+        for(int j=1;j<n;j++){
+            int index=(i+j)%n;
+            if(arr[index]>arr[i]){
+                next=arr[index];
+                break;
+            }
+        }
+        cout<<next<<" ";
+    }
+} //TC=O(n^2) SC=O(n)
+//For Optimal Approach we will use a stack to keep track of the elements for which we haven't found the next greater element yet.
+void nextGreaterElementCircularOptimal(int arr[], int n){
+    stack<int> st;
+    int res[n];
+    for(int i=2*n-1;i>=0;i--){
+        while(!st.empty() && st.top()<=arr[i%n]){
+            st.pop();
+        }
+        if(st.empty()){
+            res[i%n]=-1;
+        }
+        else{
+            res[i%n]=st.top();
+        }
+        st.push(arr[i%n]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<res[i]<<" ";
+    }
+} //TC=O(2n) SC=O(2n)
 int main(){
     int n;
     cout<<"Enter the size of the array: ";
@@ -49,6 +84,12 @@ int main(){
     cout<<endl;
     cout<<"Next Greater Element is: ";
     nextGreaterElementOptimal(arr,n);
+    cout<<endl;
+    cout<<"Next Greater Element in Circular Array is: ";
+    nextGreaterElementCircularBruteForce(arr,n);
+    cout<<endl;
+    cout<<"Next Greater Element in Circular Array is: ";
+    nextGreaterElementCircularOptimal(arr,n);
     cout<<endl;
     return 0;
 }
