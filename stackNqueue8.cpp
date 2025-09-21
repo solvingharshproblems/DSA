93558f1e1980848f10f1a5f1aa282177cd4ba271
Harsh <harshtyagi0008@icloud.com>
2025-09-21 19:32:56 +0530

    add stack & queue part 8, problem 2

--- stackNqueue8.cpp
+++ stackNqueue8.cpp
@@ -40,6 +40,68 @@ list<int> SmallerElementOptimal(int arr[],int n){
     }
     return nse;
 } //TC=O(2n) SC=O(2n)
+//Problem 2: Sum of Subarray Ranges
+//We need to return the sum of subarray difference between largest and smallest element 
+//For Brute Force Approach we can generate all subarrays and find the difference between largest and smallest element in each subarray and add it to the result.
+long long SubArrayRangesBruteForce(int arr[],int n){
+    long long sum=0;
+    for(int i=0;i<n;i++){
+        int max1=arr[i];
+        int min1=arr[i];
+        for(int j=i;j<n;j++){
+            max1=max(max1,arr[j]);
+            min1=min(min1,arr[j]);
+            sum+=max1-min1;
+        }
+    }
+    return sum;
+} //TC=O(n^2) SC=O(1)
+//For Optimal Approach will return the sum of subarray maximums minus the sum of subarray minimums from it.
+int sumOfSubarrayMinimums(int arr[], int n){
+    stack<int> st;
+    long long sum=0;
+    for(int i=0;i<=n;i++){
+        while(!st.empty() && (i==n || arr[st.top()]>=arr[i])){
+            int j=st.top();
+            st.pop();
+            int k;
+            if(st.empty()){
+                k=-1;
+            }
+            else{
+                k=st.top();
+            }
+            sum+=arr[j]*(i-j)*(j-k);
+            sum%=1000000007;
+        }
+        st.push(i);
+    }
+    return sum;
+} 
+int SumOfSubarrayMaximums(int arr[], int n){
+    stack<int> st;
+    long long sum=0;
+    for(int i=0;i<=n;i++){
+        while(!st.empty() && (i==n || arr[st.top()]<=arr[i])){
+            int j=st.top();
+            st.pop();
+            int k;
+            if(st.empty()){
+                k=-1;
+            }
+            else{
+                k=st.top();
+            }
+            sum+=arr[j]*(i-j)*(j-k);
+            sum%=1000000007;
+        }
+        st.push(i);
+    }
+    return sum;
+}
+long long SubArrayRangesOptimal(int arr[],int n){
+    return SumOfSubarrayMaximums(arr,n)-sumOfSubarrayMinimums(arr,n);
+} //TC=O(10n) SC=O(10n)
 int main(){
     int n;
     cout<<"Enter the size of the array: ";
@@ -58,5 +120,7 @@ int main(){
         cout<<*it<<" ";
     }
     cout<<endl;
+    cout<<"Sum of Subarray Ranges using is: "<<SubArrayRangesBruteForce(arr,n)<<endl;
+    cout<<"Sum of Subarray Ranges using is: "<<SubArrayRangesOptimal(arr,n)<<endl;
     return 0;
 }
