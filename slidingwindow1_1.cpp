59cb8915188398a61119dfe6ada025662e254e2a
Harsh <harshtyagi0008@icloud.com>
2025-10-04 14:59:58 +0530

    add sliding window part 1, 2nd pattern of solving problems

--- slidingwindow1_1.cpp
+++ slidingwindow1_1.cpp
@@ -20,6 +20,61 @@ void FixedSizeWindow(vector<int> arr,int k){
     }
     cout<<maxSum;
 } // TC=O(2n) SC=O(1)
+//2. Longest subarray(substring with <condition>)
+//Longest subarray with sum<=k
+//For Brute Force Approach, we can generate all of the subarrays and check if their sum is less than or equal to k
+void LongestSubarrayBruteForce(vector<int> arr,int k){
+    int n=arr.size();
+    int sum=0;
+    int maxLength=0;
+    for(int i=0;i<n;i++){
+        sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(sum<=k){
+                maxLength=max(maxLength,j-i+1);
+            }
+            else if(sum>k){
+                break;
+            }
+        }
+    }
+    cout<<maxLength;
+} // TC=O(n^2) SC=O(1)
+//For Better Approach, we can use sliding window technique
+void LongestSubarrayBetter(vector<int> arr,int k){
+    int n=arr.size();
+    int l=0,r=0;
+    int sum=0;
+    int maxLength=0;
+    while(r<n){
+        sum+=arr[r];
+        while(sum>k){
+            sum-=arr[l];
+            l++;
+        }
+        maxLength=max(maxLength,r-l+1);
+        r++;
+    }
+    cout<<maxLength;
+} // TC=O(2n) SC=O(1)
+//For Optimal Approach, we will compute 2 of these steps into a single step of better approach
+void LongestSubarrayOptimal(vector<int> arr,int k){
+    int n=arr.size();
+    int l=0,r=0;
+    int sum=0;
+    int maxLength=0;
+    while(r<n){
+        sum+=arr[r];
+        if(sum>k){
+            sum-=arr[l];
+            l++;
+        }
+        maxLength=max(maxLength,r-l+1);
+        r++;
+    }
+    cout<<maxLength;
+} // TC=O(n) SC=O(1)
 int main(void){
     int n;
     cout<<"Enter the size of array: ";
@@ -33,4 +88,10 @@ int main(void){
     cout<<"Enter the size of window: ";
     cin>>k;
     FixedSizeWindow(arr,k);
+    cout<<endl;
+    LongestSubarrayBruteForce(arr,k);
+    cout<<endl;
+    LongestSubarrayBetter(arr,k);
+    cout<<endl; 
+    LongestSubarrayOptimal(arr,k);
 }
