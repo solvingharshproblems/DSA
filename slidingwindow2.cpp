7c31a0111b23f2bcd83e02a416c80bcddc6b7355
Harsh <harshtyagi0008@icloud.com>
2025-10-05 11:43:14 +0530

    add sliding window part 2, problem 2

--- slidingwindow2.cpp
+++ slidingwindow2.cpp
@@ -46,7 +46,7 @@ void LongestSubstringBetter(string s){
     }
     cout<<"Length of longest substring without repeating characters is: "<<maxLength;
 } // TC=O(n^2) SC=O(n)
-//For Optimal Approach, we can use sliding window technique
+//For Optimal Approach, we will use sliding window technique
 void LongestSubstringOptimal(string s){
     int n=s.size();
     int maxLength=0;
@@ -67,7 +67,77 @@ void LongestSubstringOptimal(string s){
     }
     cout<<"Length of longest substring without repeating characters is: "<<maxLength;
 } // TC=O(n) SC=O(n)
+//Problem 2: Maximum Consecutive Ones III
+//Given a binary array nums and an integer k, return the length of maximum number of consecutive 1s in the array if you can flip at most k 0s.
+//We can convert this question to find the longest subarray with at most k 0s
+//For Brute Force Approach, we can generate all subarrays and check if they have at most k 0s. If they do, we can update the maxLength
+void MaxConsecutiveOnesBruteForce(vector<int> arr,int k){
+    int n=arr.size();
+    int maxLength=0;
+    for(int i=0;i<n;i++){
+        int zeroCount=0;
+        for(int j=i;j<n;j++){
+            if(arr[j]==0){
+                zeroCount++;
+            }
+            if(zeroCount>k){
+                break;
+            }
+            int length=j-i+1;
+            maxLength=max(maxLength,length);
+        }
+    }
+    cout<<"Length of longest subarray with at most "<<k<<" 0s is: "<<maxLength;
+} // TC=O(n^2) SC=O(1)
+//For Better Approach, we can use sliding window technique
+//We can maintain a window with at most k 0s and update the maxLength
+void MaxConsecutiveOnesBetter(vector<int> arr,int k){
+    int n=arr.size();
+    int maxLength=0;
+    int left=0;
+    int right=0;
+    int zeroCount=0;
+    while(right<n){
+        if(arr[right]==0){
+            zeroCount++;
+        }
+        while(zeroCount>k){
+            if(arr[left]==0){
+                zeroCount--;
+            }
+            left++;
+        }
+        int length=right-left+1;
+        maxLength=max(maxLength,length);
+        right++;
+    }
+    cout<<"Length of longest subarray with at most "<<k<<" 0s is: "<<maxLength;
+} // TC=O(2n) SC=O(1)
+//For Optimal Approach, we will use sliding window technique and we will try to remove the inner while loop
+void MaxConsecutiveOnesOptimal(vector<int> arr,int k){
+    int n=arr.size();
+    int maxLength=0;
+    int left=0;
+    int right=0;
+    int zeroCount=0;
+    while(right<n){
+        if(arr[right]==0){
+            zeroCount++;
+        }
+        if(zeroCount>k){
+            if(arr[left]==0){
+                zeroCount--;
+            }
+            left++;
+        }
+        int length=right-left+1;
+        maxLength=max(maxLength,length);
+        right++;
+    }
+    cout<<"Length of longest subarray with at most "<<k<<" 0s is: "<<maxLength;
+} // TC=O(n) SC=O(1)
 int main(){
+    /*
     string s;
     cout<<"Enter the string: ";
     cin>>s;
@@ -76,5 +146,22 @@ int main(){
     LongestSubstringBetter(s);
     cout<<endl;
     LongestSubstringOptimal(s);
+    */
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements of array: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int k;
+    cout<<"Enter the number of 0s that can be flipped: ";
+    cin>>k;
+    MaxConsecutiveOnesBruteForce(arr,k);
+    cout<<endl;
+    MaxConsecutiveOnesBetter(arr,k);
+    cout<<endl;
+    MaxConsecutiveOnesOptimal(arr,k);
     return 0;
 }
