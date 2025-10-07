30335c2b344117bf3453a0634f4f0187f1bf405f
Harsh <harshtyagi0008@icloud.com>
2025-10-07 23:02:38 +0530

    add sliding window part 4, problem 2

--- slidingwindow4.cpp
+++ slidingwindow4.cpp
@@ -43,11 +43,77 @@ void CountSubstringsOptimal(string s){
     }
     cout<<"Count of substrings containing all three characters is: "<<count<<endl;
 } // TC=O(n) SC=O(1)
+//Problem 2: Longest repeating character replacement
+//For Brute Force Approach, we can generate all substrings and check if we can replace k characters to make all characters same. If we can, we can update the maxLength
+void LongestRepeatingCharacterBruteForce(string s, int k){
+    int n=s.size();
+    int maxLength=0;
+    for(int i=0;i<n;i++){
+        int hash[26]={0};
+        int maxCount=0;
+        for(int j=i;j<n;j++){
+            hash[s[j]-'A']++;
+            maxCount=max(maxCount,hash[s[j]-'A']);
+            if(j-i+1-maxCount<=k){
+                maxLength=max(maxLength,j-i+1);
+            }
+        }
+    }
+    cout<<"Longest repeating character replacement is: "<<maxLength<<endl;
+} // TC=O(n^2) SC=O(1)
+//For Better Approach, we can use sliding window technique
+void LongestRepeatingCharacterBetter(string s, int k){
+    int left=0;
+    int right=0;
+    int n=s.size();
+    int hash[26]={0};
+    int maxCount=0;
+    int maxLength=0;
+    while(right<n){
+        hash[s[right]-'A']++;
+        maxCount=max(maxCount,hash[s[right]-'A']);
+        while(right-left+1-maxCount>k){
+            hash[s[left]-'A']--;
+            left++;
+        }
+        maxLength=max(maxLength,right-left+1);
+        right++;
+    }
+    cout<<"Longest repeating character replacement is: "<<maxLength<<endl;
+} // TC=O(2n) SC=O(1)
+//For Optimal Approach, we will reduce the time complexity of better approach by removing the inner while loop
+void LongestRepeatingCharacterOptimal(string s, int k){
+    int left=0;
+    int right=0;
+    int n=s.size();
+    int maxCount=0;
+    int maxLength=0;
+    int hash[26]={0};
+    while(right<n){
+        hash[s[right]-'A']++;
+        maxCount=max(maxCount,hash[s[right]-'A']);
+        if(right-left+1-maxCount>k){
+            hash[s[left]-'A']--;
+            left++;
+        }
+        maxLength=max(maxLength,right-left+1);
+        right++;
+    }
+    cout<<"Longest repeating character replacement is: "<<maxLength<<endl;
+} // TC=O(n) SC=O(1)
 int main(){
     string s;
     cout<<"Enter the string: ";
     cin>>s;
+    /*
     CountSubstringsBruteForce(s);
     CountSubstringsOptimal(s);
+    */
+    int k;
+    cout<<"Enter the value of k: ";
+    cin>>k;
+    LongestRepeatingCharacterBruteForce(s,k);
+    LongestRepeatingCharacterBetter(s,k);
+    LongestRepeatingCharacterOptimal(s,k);
     return 0;
 }
