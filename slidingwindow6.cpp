e41b718cc736afc87e70a551d766b70b5f3d237f
Harsh <harshtyagi0008@icloud.com>
2025-10-09 19:36:24 +0530

    add sliding window part 6, problem 2

--- slidingwindow6.cpp
+++ slidingwindow6.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <string.h>
 using namespace std;
 //Problem 1: Subarrays with K Different Integers
 //For Brute Force Approach, we can use a nested loop to find all the subarrays and check if they have k different integers
@@ -44,7 +45,77 @@ int CountSubarraysOptimal(vector<int>& arr,int k){
 void PrintSubarraysOptimal(vector<int>& arr,int k){
     cout<<"Count of subarrays with k different integers is: "<<CountSubarraysOptimal(arr,k)-CountSubarraysOptimal(arr,k-1)<<endl;
 } // TC=O(n) SC=O(n)
+//Problem 2: Minimum Window Substring
+//Given two strings s and t, return the minimum window in s which will contain all the characters in t
+//For Brute Force Approach, we can use a nested loop to find all the substrings and check if they contain all the characters in t
+void MinWindowSubstringBruteForce(string s,string t){
+    int n=s.size();
+    int m=t.size();
+    int minLength=INT_MAX;
+    int index=-1;
+    for(int i=0;i<n;i++){
+       int hash[256]={0};
+       for(int j=0;j<m;j++) hash[t[j]]++;
+       int count=m;
+       for(int j=i;j<n;j++){
+           if(hash[s[j]]>0){
+               count--;
+           }
+           hash[s[j]]--;
+           if(count==0){
+               if(j-i+1<minLength){
+                   minLength=j-i+1;
+                   index=i;
+               }
+               break;
+           }
+       }
+    }
+    if(index==-1){
+        cout<<"No such window exists"<<endl;
+    }
+    else{
+        cout<<"Minimum window substring is: "<<s.substr(index,minLength)<<endl;
+    }
+} // TC=O(n^2) SC=O(n)
+//For Optimal Approach, we will use sliding window technique and hashmap to store the count of characters in t
+int MinWindowSubstringOptimal(string s,string t){
+    int n=s.size();
+    int m=t.size();
+    int hash[256]={0};
+    int l=0,r=0,count=0;
+    int minLen=INT_MAX;
+    int sIndex=-1;
+    for(int i=0;i<m;i++) hash[t[i]]++;
+    count=m;
+    while(r<n){
+        if(hash[s[r]]>0){
+            count--;
+        }
+        hash[s[r]]--;
+        while(count==0){
+            if(r-l+1<minLen){
+                minLen=r-l+1;
+                sIndex=l;
+            }
+            hash[s[l]]++;
+            if(hash[s[l]]>0){
+                count++;
+            }
+            l++;
+        }
+        r++;
+    }
+    if(sIndex==-1){
+        return -1;
+    }
+    else{
+        cout<<"Minimum window substring is: "<<s.substr(sIndex,minLen)<<endl;
+        return minLen;
+    }
+} // TC=O(2n) SC=O(n)
 int main(){
+    /*
     int n;
     cout<<"Enter the size of the array: ";
     cin>>n;
@@ -58,5 +129,13 @@ int main(){
     cin>>k;
     CountSubarraysBruteForce(arr,k);
     PrintSubarraysOptimal(arr,k);
+    */
+    string s,t;
+    cout<<"Enter the string s: ";
+    cin>>s;
+    cout<<"Enter the string t: ";
+    cin>>t;
+    MinWindowSubstringBruteForce(s,t);
+    MinWindowSubstringOptimal(s,t);
     return 0;
 }
