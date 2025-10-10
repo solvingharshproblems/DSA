b0f7f7a631f5614dafd77ca9e226ee6aeb672356
Harsh <harshtyagi0008@icloud.com>
2025-10-10 18:39:53 +0530

    add greey algorithm part 1, problem 2

--- greedyalgorithm1.cpp
+++ greedyalgorithm1.cpp
@@ -23,7 +23,47 @@ void AssignCookiesOptimal(vector<int> g,vector<int> s){
     }
     cout<<"Maximum number of content children: "<<count<<endl;
 } // TC=O(nlogn+mlogm+n+m) SC=O(1)
+//Problem 2: Lemonade Change
+//At a lemonade stand, each lemonade costs $5.
+//Customers are standing in a queue to buy from you, and order one at a time (in the order specified by bills).
+//Each customer will only buy one lemonade and pay with either a $5, $10, or $20 bill.
+//You must provide the correct change to each customer, so that the net transaction is that the customer pays $5.
+//Note that you don't have any change in hand at first.
+//Return true if and only if you can provide every customer with correct change.
+bool LemonadeChangeOptimal(vector<int> bills){
+    int five=0,ten=0,twenty=0;
+    for(int i=0;i<bills.size();i++){
+        if(bills[i]==5){
+            five++;
+        }
+        else if(bills[i]==10){
+            if(five>0){
+                five--;
+                ten++;
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            if(ten>0 && five>0){
+                ten--;
+                five--;
+                twenty++;
+            }
+            else if(five>=3){
+                five-=3;
+                twenty++;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    return true;
+} // TC=O(n) SC=O(1)
 int main(){
+    /*
     int n;
     cout<<"Enter the number of children: ";
     cin>>n;
@@ -41,5 +81,20 @@ int main(){
         cin>>s[i];
     }
     AssignCookiesOptimal(g,s);
+    */
+    int n;
+    cout<<"Enter the number of customers: ";
+    cin>>n;
+    vector<int> bills(n);
+    cout<<"Enter the bills of customers: ";
+    for(int i=0;i<n;i++){
+        cin>>bills[i];
+    }
+    if(LemonadeChangeOptimal(bills)){
+        cout<<"True"<<endl;
+    }
+    else{
+        cout<<"False"<<endl;
+    }
     return 0;
 }
