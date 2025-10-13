3f84f38fbccc8863e8a9f689186c3562e1852036
Harsh <harshtyagi0008@icloud.com>
2025-10-13 16:17:05 +0530

    add greedy algorithm part 4, problem 2

--- greedyalgorithm4.cpp
+++ greedyalgorithm4.cpp
@@ -20,6 +20,39 @@ void NonOverlappingIntervals(vector<pair<int,int>> arr){
     }
     cout<<"Minimum number of intervals to be removed: "<<n-count<<endl;
 } // TC=O(nlogn+n) SC=O(1)
+//Problem 2: Insert Interval
+//Return the resulting array of intervals after inserting the new interval where the array of intervals is non-overlapping 
+//Note: The intervals are given in sorted order according to their start times
+//For Optimal Approach, if there is any overlap, we will select minimum start time and maximum end time from the overlapping intervals
+//Else we will simply insert the new interval at the correct position
+bool CompareInterval2(pair<int,int> value1,pair<int,int> value2){
+    return value1.first<value2.first;
+}
+void InsertInterval(vector<pair<int,int>> arr,pair<int,int> newInterval){
+    int n=arr.size();
+    vector<pair<int,int>> ans;
+    int i=0;
+    //Adding all the intervals which end before the new interval starts
+    while(i<n && arr[i].second<newInterval.first){
+        ans.push_back(arr[i]);
+        i++;
+    }
+    //Merging all the overlapping intervals
+    while(i<n && arr[i].first<=newInterval.second){
+        newInterval.first=min(newInterval.first,arr[i].first);
+        newInterval.second=max(newInterval.second,arr[i].second);
+        i++;
+    }
+    ans.push_back(newInterval);
+    //Adding all the intervals which start after the new interval ends
+    while(i<n){
+        ans.push_back(arr[i]);
+        i++;
+    }
+    for(int i=0;i<ans.size();i++){
+        cout<<"["<<ans[i].first<<","<<ans[i].second<<"] ";
+    }
+} // TC=O(n) SC=O(n) for storing the answer
 int main(){
     int n;
     cout<<"Enter the number of intervals: ";
@@ -30,5 +63,10 @@ int main(){
         cin>>arr[i].first>>arr[i].second;
     }
     NonOverlappingIntervals(arr);
+    pair<int,int> newInterval;
+    cout<<"Enter the start and end times of the new interval: ";
+    cin>>newInterval.first>>newInterval.second;
+    InsertInterval(arr,newInterval);
+    cout<<endl;
     return 0;
 }
