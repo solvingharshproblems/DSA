28e5e9c7db5d017a696ef04faaeb07dfced6b023
Harsh <harshtyagi0008@icloud.com>
2025-10-14 17:22:02 +0530

    add greedy algorithm part 5, problem 2

--- greedyalgorithm5.cpp
+++ greedyalgorithm5.cpp
@@ -23,7 +23,36 @@ void MinimumPlatforms(vector<int> arrival,vector<int> departure){
     }
     cout<<"Minimum number of platforms required: "<<maxPlatforms<<endl;
 } // TC=O(nlogn+nlogn+n) SC=O(1)
+//Problem 2: Fractional Knapsack Problem
+//Given the weights and values of n items, we have to put these items in a knapsack of capacity W to get the maximum total value in the knapsack
+//For Optimal Approach, we will calculate the value/weight ratio for each item and sort the items in decreasing order of this ratio
+//Then we will keep adding the items to the knapsack until we reach its capacity, if we cannot add the complete item, we will add the fractional part of it
+void FractionalKnapsack(vector<int> weight,vector<int> value,int W){
+    int n=weight.size();
+    vector<pair<double,pair<int,int>>> arr(n);
+    for(int i=0;i<n;i++){
+        double ratio=(double)value[i]/(double)weight[i];
+        arr[i]={ratio,{weight[i],value[i]}};
+    }
+    sort(arr.begin(),arr.end(),greater<pair<double,pair<int,int>>>());
+    double totalValue=0.0;
+    for(int i=0;i<n;i++){
+        if(W==0){
+            break;
+        }
+        if(arr[i].second.first<=W){
+            W-=arr[i].second.first;
+            totalValue+=arr[i].second.second;
+        }
+        else{
+            totalValue+=arr[i].first*W;
+            W=0;
+        }
+    }
+    cout<<"Maximum total value in the knapsack: "<<totalValue<<endl;
+} // TC=O(nlogn+n) SC=O(n) for storing the array
 int main(){
+    /*
     int n;
     cout<<"Enter the number of trains: ";
     cin>>n;
@@ -38,5 +67,23 @@ int main(){
         cin>>departure[i];
     }
     MinimumPlatforms(arrival,departure);
+    */
+    int n;
+    cout<<"Enter the number of items: ";
+    cin>>n;
+    vector<int> weight(n);
+    vector<int> value(n);
+    cout<<"Enter the weights of items: ";
+    for(int i=0;i<n;i++){
+        cin>>weight[i];
+    }
+    cout<<"Enter the values of items: ";
+    for(int i=0;i<n;i++){
+        cin>>value[i];
+    }
+    int W;
+    cout<<"Enter the capacity of the knapsack: ";
+    cin>>W;
+    FractionalKnapsack(weight,value,W);
     return 0;
 }
