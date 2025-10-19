ef9650a172dae4d59f8ddda209768989bb0e4cae
Harsh <harshtyagi0008@icloud.com>
2025-10-19 13:16:46 +0530

    add trees part 4, morris traversal

--- trees4.cpp
+++ trees4.cpp
@@ -59,6 +59,52 @@ void PostOrder2(Node* root){
         }
     }
 } // TC=O(n) SC=O(h) where h is the height of the tree
+//3. Preorder, Inorder and Postorder Traversals (Morris Traversal)
+void MorrisTraversal(Node* root){
+    stack<pair<Node*,int>> s;
+    s.push({root,1});
+    vector<int> pre,in,post;
+    if(root==nullptr){
+        return;
+    }
+    while(!s.empty()){
+        auto it=s.top();
+        s.pop();
+        if(it.second==1){
+            pre.push_back(it.first->data);
+            it.second++;
+            s.push(it);
+            if(it.first->left!=nullptr){
+                s.push({it.first->left,1});
+            }
+        }
+        else if(it.second==2){
+            in.push_back(it.first->data);
+            it.second++;
+            s.push(it);
+            if(it.first->right!=nullptr){
+                s.push({it.first->right,1});
+            }
+        }
+        else{
+            post.push_back(it.first->data);
+        }
+    }
+    cout<<"Preorder Traversal: ";
+    for(int val:pre){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+    cout<<"Inorder Traversal: ";
+    for(int val:in){
+        cout<<val<<" "; 
+    }
+    cout<<endl;
+    cout<<"Postorder Traversal: ";
+    for(int val:post){
+        cout<<val<<" ";
+    }
+} // TC=O(3n) SC=O(4h) where h is the height of the tree
 int main(){
     Node* root=new Node(1);
     root->left=new Node(2);
@@ -73,5 +119,7 @@ int main(){
     PostOrder1(root);
     cout<<endl;
     PostOrder2(root);
+    cout<<endl;
+    MorrisTraversal(root);
     return 0;
 }
