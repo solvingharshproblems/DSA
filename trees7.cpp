3c15f05ce018fc0bf8b9af181afeae2d9642fa3c
Harsh <harshtyagi0008@icloud.com>
2025-10-23 11:35:06 +0530

    add trees part 7, problem 2

--- trees7.cpp
+++ trees7.cpp
@@ -21,7 +21,45 @@ bool isIdentical(Node* root1,Node* root2){
     }
     return (root1->data==root2->data) && isIdentical(root1->left,root2->left) && isIdentical(root1->right,root2->right);
 } // TC=O(min(n1,n2)) SC=O(h) where h is the height of the tree
+//Problem 2: Zig-Zag traversal
+//For Optimal Approach, we can use level order traversal where at the iteration of 2 levels, we will reverse the nodes
+void ZigZag(Node* root){
+    vector<vector<int>> result;
+    if(root==nullptr){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size=q.size();
+        vector<int> currentLevel;
+        for(int i=0;i<size;i++){
+            Node* currentNode=q.front();
+            q.pop();
+            if(currentNode->left!=nullptr){
+                q.push(currentNode->left);
+            }
+            if(currentNode->right!=nullptr){
+                q.push(currentNode->right);
+            }
+            currentLevel.push_back(currentNode->data);
+        }
+        result.push_back(currentLevel);
+    }
+    for(int i=0;i<result.size();i++){
+        if(i%2!=0){
+            reverse(result[i].begin(),result[i].end());
+        }
+    }
+    for(const auto& level:result){
+        for(int val:level){
+            cout<<val<<" ";
+        }
+        cout<<endl;
+    }
+} // TC=O(n) SC=O(n) where n is the number of nodes
 int main(){
+    /*
     Node* root1=new Node(1);
     root1->left=new Node(2);
     root1->right=new Node(3);
@@ -38,5 +76,13 @@ int main(){
     else{
         cout<<"The trees are not identical"<<endl;
     }
+    */
+    Node* root=new Node(1);
+    root->left=new Node(2); 
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->right=new Node(6);
+    ZigZag(root);
     return 0;
 }
