72b21b3e809a184401741b4b2e650505c9d9db71
Harsh <harshtyagi0008@icloud.com>
2025-10-26 15:06:19 +0530

    add trees part 10, problem 2

--- trees10.cpp
+++ trees10.cpp
@@ -11,7 +11,7 @@ typedef struct Node{
     }
 }Node;
 //Problem 1: Root to node path in a binary tree
-//For Optimal Approach, we can use inorder recursive traversal to find the path from root to the given node.
+//For Optimal Approach, we will use inorder recursive traversal to find the path from root to the given node.
 void RootToNodePath(Node* root,int key){
     if(root==nullptr){
         return;
@@ -28,6 +28,23 @@ void RootToNodePath(Node* root,int key){
     RootToNodePath(root->right,key);
     path.pop_back();
 } // TC=O(n) SC=O(h)
+//Problem 2: Lowest common ancestor in a binary tree
+//A node is the lowest common ancestor of two nodes n1 and n2 if it is the deepest node that has both n1 and n2 as descendants.
+//For Optimal Approach, we will use preorder recursive traversal to find the lowest common ancestor of the two given nodes.
+Node* LCA(Node* root,int n1,int n2){
+    if(root==nullptr){
+        return nullptr;
+    }
+    if(root->data==n1 || root->data==n2){
+        return root;
+    }
+    Node* leftLCA=LCA(root->left,n1,n2);
+    Node* rightLCA=LCA(root->right,n1,n2);
+    if(leftLCA && rightLCA){
+        return root;
+    }
+    return (leftLCA!=nullptr)?leftLCA:rightLCA;
+} // TC=O(n) SC=O(h)
 int main(){
     Node* root=new Node(1);
     root->left=new Node(2);
@@ -40,5 +57,15 @@ int main(){
     cout<<"Root to node path for "<<key<<": ";
     RootToNodePath(root,key);
     cout<<endl;
+    root->right->right=new Node(9);
+    root->right->left=new Node(8);
+    int n1=4,n2=7;
+    Node* lca=LCA(root,n1,n2);
+    if(lca!=nullptr){
+        cout<<"LCA of "<<n1<<" and "<<n2<<" is: "<<lca->data<<endl;
+    }
+    else{
+        cout<<"LCA does not exist!"<<endl;
+    }
     return 0;
 }
