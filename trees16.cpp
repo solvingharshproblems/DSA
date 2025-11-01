f6c9ea6c060d69e7c62333241c481eb05f7e95c9
Harsh <harshtyagi0008@icloud.com>
2025-11-01 12:50:05 +0530

    add trees part 16, problem 2

--- trees16.cpp
+++ trees16.cpp
@@ -36,6 +36,40 @@ Node* InsertInBST(Node* root,int key){
     }
     return root;
 } // TC=O(h) SC=O(1), h is the height of the tree
+//Problem 2: Delete in a BST
+//For Optimal Approach, there will be 2 cases 
+//1. Node to be deleted has one child or no child
+//2. Node to be deleted has two children
+Node* DeleteInBST(Node* root,int key){
+    if(root==nullptr){
+        return root;
+    }
+    if(key<root->data){
+        root->left=DeleteInBST(root->left,key);
+    }
+    else if(key>root->data){
+        root->right=DeleteInBST(root->right,key);
+    }
+    else{
+        if(root->left==nullptr){
+            Node* temp=root->right;
+            delete root;
+            return temp;
+        }
+        else if(root->right==nullptr){
+            Node* temp=root->left;
+            delete root;
+            return temp;
+        }
+        Node* temp=root->right;
+        while(temp->left){
+            temp=temp->left;
+        }
+        root->data=temp->data;
+        root->right=DeleteInBST(root->right,temp->data);
+    }
+    return root;
+} // TC=O(h) SC=O(h), h is the height of the tree
 void Inorder(Node* root) {
     if (!root) return;
     Inorder(root->left);
@@ -48,6 +82,10 @@ int main(){
     root->right=new Node(15);
     root=InsertInBST(root,7);
     cout<<"Inorder Traversal after Insertion: ";
+    Inorder(root);
+    cout<<endl;
+    root=DeleteInBST(root,5);
+    cout<<"Inorder Traversal after Deletion: ";
     Inorder(root);
     cout<<endl;
     return 0;
