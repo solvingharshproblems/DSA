ad638cccd4a93360a3d664f0bfa1ebeb3439deb1
Harsh <harshtyagi0008@icloud.com>
2025-11-09 11:08:18 +0530

    add graphs part 4, problem 2

--- graphs4.cpp
+++ graphs4.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //Problem 1: Floor Fill Algorithm
 //Given a 2D grid representing an image, a starting pixel, and a new color, perform a "flood fill" to change the color of the starting pixel and all connected pixels
-//For Optimal Approach, we can use DFS to traverse all connected pixels and change their color.
+//For Optimal Approach, we will use DFS to traverse all connected pixels and change their color.
 void fillDFS(int row,int col,vector<vector<int>>& image,int newColor,int originalColor){
     int n=image.size();
     int m=image[0].size();
@@ -23,12 +23,55 @@ vector<vector<int>> floodFill(vector<vector<int>>& image,int sr,int sc,int newCo
     fillDFS(sr,sc,image,newColor,originalColor);
     return image;
 } // TC=O(n*m) SC=O(n*m) for storing answers in a matrix + O(n*m) for recursion stack
+//Problem 2: Rotten Oranges
+//Given a 2D grid representing oranges, where 0=empty cell, 1=fresh orange, 2=rotten orange, find the minimum time required to rot all fresh
+//For Optimal Approach, we will declare a queue to store the coordinates of all rotten oranges and time taken to rot adjacent fresh oranges then use BFS to traverse the grid.
+int orangesRotting(vector<vector<int>>& grid){
+    int n=grid.size();
+    int m=grid[0].size();
+    queue<pair<pair<int,int>,int>> q;
+    int fresh=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+                q.push({{i,j},0});
+            }
+            else if(grid[i][j]==1){
+                fresh++;
+            }
+        }
+    }
+    int time=0;
+    int delRow[]={-1,0,1,0};
+    int delCol[]={0,1,0,-1};
+    while(!q.empty()){
+        int row=q.front().first.first;
+        int col=q.front().first.second;
+        int t=q.front().second;
+        q.pop();
+        time=max(time,t);
+        for(int k=0;k<4;k++){
+            int nrow=row+delRow[k];
+            int ncol=col+delCol[k];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1){
+                grid[nrow][ncol]=2;
+                fresh--;
+                q.push({{nrow,ncol},t+1});
+            }
+        }
+    }
+    if(fresh>0){
+        return -1;
+    }
+    return time;
+} // TC=O(n*m) SC= O(n*m) for storing answers in a matrix + O(n*m) for queue
 int main(){
     int rows,cols;
     cout<<"Enter the number of rows:";
     cin>>rows;
     cout<<"Enter the number of columns:";
     cin>>cols;
+    /*
     vector<vector<int>> image(rows,vector<int>(cols));;
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
@@ -49,5 +92,14 @@ int main(){
         }
         cout << endl;
     }
+    */
+    vector<vector<int>> grid(rows,vector<int>(cols));;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            cin>>grid[i][j];
+        }
+    }
+    int time=orangesRotting(grid);
+    cout<<"Minimum time to rot all oranges: "<<time<<endl;  
     return 0;
 }
