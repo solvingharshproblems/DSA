fc0a806bbb0882ca3df581815255b34987449692
Harsh <harshtyagi0008@icloud.com>
2026-01-22 11:29:58 +0530

    add graphs part 16, problem 1

---
+++ graphs16.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+//Problem 1: Shortest Distance in a Binary Maze
+//Given a binary matrix, find the shortest distance from the source cell to the destination cell,
+//moving only in 4 possible directions (up, down, left, right) and only through cells with value 1.
+//For Optimal Approach, we will apply Dijkstra's Algorithm using a queue instead of a priority queue as all edges have the same weight of 1.
+//And a 2-D distance array to keep track of the shortest distance to each cell by initializing all distances to infinity except the source cell which is initialized to 0.
+int shortestDistance(vector<vector<int>>& grid,pair<int,int> source,pair<int,int> destination){
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
+    dist[source.first][source.second]=0;
+    queue<pair<int,pair<int,int>>> q;
+    q.push({0,{source.first,source.second}});
+    int delRow[]={-1,0,1,0};
+    int delCol[]={0,1,0,-1};
+    while(!q.empty()){
+        auto it=q.front();
+        q.pop();    
+        int distance=it.first;
+        int row=it.second.first;
+        int col=it.second.second;
+        for(int i=0;i<4;i++){
+            int newRow=row+delRow[i];
+            int newCol=col+delCol[i];
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && grid[newRow][newCol]==1){
+                if(distance+1<dist[newRow][newCol]){
+                    dist[newRow][newCol]=distance+1;
+                    if(newRow==destination.first && newCol==destination.second){
+                        return distance+1;
+                    }
+                    q.push({dist[newRow][newCol],{newRow,newCol}});
+                }
+            }
+        }
+    }
+    return -1;
+} // TC=O(N*M log(N*M)) SC=O(N*M)
+int main(){
+    int n=5,m=5;
+    vector<vector<int>> grid={
+        {1, 0, 1, 1, 1},
+        {1, 0, 1, 0, 1},
+        {1, 1, 1, 0, 1},
+        {0, 0, 1, 1, 1},
+        {1, 0, 0, 0, 1}
+    };
+    pair<int,int> source={0,0};
+    pair<int,int> destination={4,4};
+    int shortestDist=shortestDistance(grid,source,destination);
+    if(shortestDist!=-1){
+        cout<<"The shortest distance from source to destination is: "<<shortestDist<<endl;
+    } 
+    else{
+        cout<<"The destination is unreachable from the source."<<endl;
+    }
+    return 0;
+}
