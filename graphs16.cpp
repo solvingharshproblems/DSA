3a9f6ff420b5871fd489806609997afd6a643e68
Harsh <harshtyagi0008@icloud.com>
2026-01-22 11:52:58 +0530

    add graphs part 16, problem 2

--- graphs16.cpp
+++ graphs16.cpp
@@ -35,6 +35,41 @@ int shortestDistance(vector<vector<int>>& grid,pair<int,int> source,pair<int,int
         }
     }
     return -1;
+} // TC=O(N*M) SC=O(N*M)
+//Problem 2: Path with Minimum Effort in a Grid
+//Given a 2D grid of heights, find a path from the top-left to the bottom-right such that the maximum absolute difference in heights between consecutive cells is minimized.
+//For Optimal Approach, we will use a modified Dijkstra's Algorithm where we minimize the maximum effort instead of the sum of efforts.
+int minEffort(vector<vector<int>>& heights){
+    int n=heights.size();
+    int m=heights[0].size();
+    vector<vector<int>> effort(n,vector<int>(m,INT_MAX));
+    effort[0][0]=0;
+    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+    pq.push({0,{0,0}});
+    int delRow[]={-1,0,1,0};
+    int delCol[]={0,1,0,-1};
+    while(!pq.empty()){
+        auto it=pq.top();
+        pq.pop();    
+        int currEffort=it.first;
+        int row=it.second.first;
+        int col=it.second.second;
+        for(int i=0;i<4;i++){
+            int newRow=row+delRow[i];
+            int newCol=col+delCol[i];
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m){
+                int nextEffort=max(currEffort,abs(heights[newRow][newCol]-heights[row][col]));
+                if(nextEffort<effort[newRow][newCol]){
+                    effort[newRow][newCol]=nextEffort;
+                    if(newRow==n-1 && newCol==m-1){
+                        return nextEffort;
+                    }
+                    pq.push({nextEffort,{newRow,newCol}});
+                }
+            }
+        }
+    }
+    return 0;
 } // TC=O(N*M log(N*M)) SC=O(N*M)
 int main(){
     int n=5,m=5;
@@ -54,5 +89,12 @@ int main(){
     else{
         cout<<"The destination is unreachable from the source."<<endl;
     }
+    vector<vector<int>> heights={
+        {1,2,2},
+        {3,8,2},
+        {5,3,5}
+    };
+    int minEffortValue=minEffort(heights);
+    cout<<"The minimum effort required to travel from top-left to bottom-right is: "<<minEffortValue<<endl;
     return 0;
 }
