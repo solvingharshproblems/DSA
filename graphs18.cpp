8ec7fed7f331b78320c2ddfd8b199582e6ddf4d9
Harsh <harshtyagi0008@icloud.com>
2026-01-24 16:18:13 +0530

    add graphs part 18, problem 2

--- graphs18.cpp
+++ graphs18.cpp
@@ -40,9 +40,46 @@ int countPaths(int n,vector<vector<int>>& roads){
     }
     return ways[n-1]%mod;
 } // TC=O(E log V) SC=O(N + E)
+//Problem 2: Bellman Ford Algorithm for Negative Weight Cycle Detection
+//It is quite similar to Dijkstra's Algorithm but works for negative weights also.
+//Note: Bellman Ford Algorithm is only applicable for directed graphs.
+//For Optimal Approach, we will relax all edges V-1 times and then check for negative weight cycles.
+//Note: Since in graph of V vertices, the maximum number of edges in any path without a cycle can be V-1.
+bool bellmanFord(int n,vector<vector<int>>& edges,int src){
+    vector<long long> dist(n,1e15);
+    dist[src]=0;
+    for(int i=0;i<n-1;i++){
+        for(auto edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int weight=edge[2];
+            if(dist[u]!=1e15 && dist[u]+weight<dist[v]){
+                dist[v]=dist[u]+weight;
+            }
+        }
+    }
+    for(auto edge:edges){
+        int u=edge[0];
+        int v=edge[1];
+        int weight=edge[2];
+        if(dist[u]!=1e15 && dist[u]+weight<dist[v]){
+            return true; //Negative weight cycle detected
+        }
+    }
+    return false; //No negative weight cycle
+} // TC=O(V*E) SC=O(N)
 int main(){
     int n=7;
     vector<vector<int>> roads={{0,6,7},{0,1,2},{1,2,3},{1,3,3},{6,3,3},{3,5,1},{6,5,1},{2,5,1},{0,4,5},{4,6,2}};
     cout<<"Number of ways to arrive at destination: "<<countPaths(n,roads)<<endl;
+    int m=5;
+    vector<vector<int>> edges={{0,1,-1},{0,2,4},{1,2,3},{1,3,2},{1,4,2},{3,2,5},{3,1,1},{4,3,-3}};
+    int src=0;
+    if(bellmanFord(m,edges,src)){
+        cout<<"Negative weight cycle detected"<<endl;
+    }
+    else{
+        cout<<"No negative weight cycle detected"<<endl;
+    }
     return 0;
 }
