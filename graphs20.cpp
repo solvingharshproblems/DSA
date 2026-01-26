7b765f358124b99d1eadae750785f926293bfb49
Harsh <harshtyagi0008@icloud.com>
2026-01-26 12:29:10 +0530

    add graphs part 20, problem 2

--- graphs20.cpp
+++ graphs20.cpp
@@ -24,6 +24,59 @@ int primMST(vector<vector<int>>& graph){
     }
     return totalWeight;
 } // TC=O(E log V) SC=O(V)
+//Problem 2: Disjoint Set 
+//Disjoint Set (also known as Union-Find) is a data structure that keeps track of a partition of a set into disjoint (non-overlapping) subsets. It supports two main operations: Find and Union.
+//Find: Determine which subset a particular element is in. This can be used to determine if two elements are in the same subset.
+//Union: Join two subsets into a single subset.
+//We will implement the Disjoint Set with two optimizations: Union by Rank and Union by Size to ensure efficient operations. 
+//Union by Rank ensures that the smaller tree is always added under the root of the larger tree, keeping the overall tree flat.
+//Union by Size ensures that the smaller set is always added under the root of the larger set.
+class DisjointSet{
+    vector<int> rank,parent,size;
+public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+        }
+    }
+    int findUPar(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return parent[node]=findUPar(parent[node]);
+    }
+    void unionByRank(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(rank[ulp_u]<rank[ulp_v]){
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u]){
+            parent[ulp_v]=ulp_u;
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+    void unionBySize(int u,int v){
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else{
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+}; // TC=O(4*alpha) SC=O(N) where alpha is the Inverse Ackermann Function which is near about constant time.
 int main(){
     int n=5;
     vector<vector<int>> graph={
@@ -34,5 +87,25 @@ int main(){
         {-1,5,7,9,0}
     };  
     cout<<"Total weight of MST is: "<<primMST(graph)<<endl;
+    DisjointSet ds(7);
+    ds.unionByRank(1,2);
+    ds.unionByRank(2,3);
+    ds.unionByRank(4,5);
+    ds.unionByRank(6,7);
+    ds.unionByRank(5,6);
+    if(ds.findUPar(3)==ds.findUPar(7)){
+        cout<<"3 and 7 are in the same set."<<endl;
+    }
+    else{
+        cout<<"3 and 7 are in different sets."<<endl;
+    }
+
+    ds.unionBySize(3,7);
+    if(ds.findUPar(3)==ds.findUPar(7)){
+        cout<<"3 and 7 are in the same set."<<endl;
+    }
+    else{
+        cout<<"3 and 7 are in different sets."<<endl;
+    }
     return 0;
 }
