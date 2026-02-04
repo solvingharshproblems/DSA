52705f38eeda9dcf561ba418a6193972fa01a609
Harsh <harshtyagi0008@icloud.com>
2026-02-04 11:38:47 +0530

    add DP part 4, problem 1

---
+++ DP4.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+using namespace std;
+//Problem 1: Ninja's Training
+//A ninja is planning his training schedule over a series of days.
+//Each day, he can perform one of three activities: Running, Fighting Practice, or Learning New Moves.
+//However, he cannot perform the same activity on two consecutive days.
+//The goal is to maximize the total merit points earned over the training period.
+//For Brute Force Approach, we can use recursion to explore all possible activity sequences.
+int DFS(vector<vector<int>>& points,int day,int index){
+    if(day==0){
+        int maxInPoints=0;
+        for(int i=0;i<=2;i++){
+            if(i!=index){
+                maxInPoints=max(maxInPoints,points[0][i]);
+            }
+        }
+        return maxInPoints;
+    }
+    int maxInPoints=0;
+    for(int i=0;i<=2;i++){
+        if(i!=index){
+            int point=points[day][i]+DFS(points,day-1,i);
+            maxInPoints=max(maxInPoints,point);
+        }
+    }
+    return maxInPoints;
+}
+int ninjaTrainingBruteForce(vector<vector<int>>& points){
+    int n=points.size();
+    return DFS(points,n-1,3);
+} // TC=O(3^N) SC=O(N)
+//For Better Approach, we can use tabulation to store the computed maximum merit points up to each day for each last activity.
+int ninjaTrainingBetter(vector<vector<int>>& points){
+    int n=points.size();
+    vector<vector<int>> dp(n,vector<int>(4,0));
+    dp[0][0]=max(points[0][1],points[0][2]);
+    dp[0][1]=max(points[0][0],points[0][2]);
+    dp[0][2]=max(points[0][0],points[0][1]);
+    dp[0][3]=max(points[0][0],max(points[0][1],points[0][2]));
+    for(int day=1;day<n;day++){
+        for(int last=0;last<=3;last++){
+            dp[day][last]=0;
+            for(int task=0;task<=2;task++){
+                if(task!=last){
+                    int point=points[day][task]+dp[day-1][task];
+                    dp[day][last]=max(dp[day][last],point);
+                }
+            }
+        }
+    }
+    return dp[n-1][3];
+} // TC=O(N*4*3) SC=O(N*4)
+//For Optimal Approach, we will use space optimization to reduce the space complexity.
+int ninjaTrainingOptimal(vector<vector<int>>& points){
+    int n=points.size();
+    vector<int> prev(4,0);      
+    prev[0]=max(points[0][1],points[0][2]);
+    prev[1]=max(points[0][0],points[0][2]);
+    prev[2]=max(points[0][0],points[0][1]);
+    prev[3]=max(points[0][0],max(points[0][1],points[0][2]));
+    for(int day=1;day<n;day++){
+        vector<int> curr(4,0);  
+        for(int last=0;last<=3;last++){
+            curr[last]=0;
+            for(int task=0;task<=2;task++){
+                if(task!=last){
+                    int point=points[day][task]+prev[task];
+                    curr[last]=max(curr[last],point);
+                }
+            }
+        }
+        prev=curr;
+    }
+    return prev[3];
+} // TC=O(N*4*3) SC=O(4)
+int main(){
+    int n;
+    cout<<"Enter number of days: ";
+    cin>>n;
+    vector<vector<int>> points(n);
+    cout<<"Enter merit points for each activity on each day:\n";
+    for(int i=0;i<n;i++){
+        points[i].resize(3);
+        for(int j=0;j<3;j++){
+            cin>>points[i][j];
+        }
+    }
+    cout<<ninjaTrainingBruteForce(points)<<endl;
+    cout<<ninjaTrainingBetter(points)<<endl;
+    cout<<ninjaTrainingOptimal(points)<<endl;
+    return 0;
+}
