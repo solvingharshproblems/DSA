8e43b26aef519a5d728b06f90c862d95bf73d920
Harsh <harshtyagi0008@icloud.com>
2026-02-12 10:56:06 +0530

    add DP part 12, problem 2

--- DP12.cpp
+++ DP12.cpp
@@ -66,6 +66,73 @@ int knapsackSpaceOptimized(vector<int>& weights,vector<int>& values,int W){
     }
     return dp[W];
 } // TC=O(N*W) SC=O(W)
+//Problem 2: Rod Cutting Problem
+//Given a rod of length n inches and an array of prices that contains prices of all pieces of size smaller than n. Determine the maximum value obtainable by cutting up the rod and selling the pieces
+//Note: This problem is similar to the Unbounded Knapsack problem where the rod length is the knapsack capacity and the piece lengths are the weights.
+//For Brute Force Approach, we can use recursion to explore all possible ways to cut the rod.
+int DFS3(vector<int>& prices,int n,int i){
+    if(i>=n || n<=0){
+        return 0;
+    }
+    int exclude=DFS3(prices,n,i+1);
+    int include=0;
+    if(i+1<=n){
+        include=prices[i]+DFS3(prices,n-(i+1),i);
+    }
+    return max(include,exclude);
+}
+int rodCuttingBruteForce(vector<int>& prices){
+    int n=prices.size();
+    return DFS3(prices,n,0);
+} // TC=O(2^N) SC=O(N)
+//For Better Approach, we can use memoization to store the results of subproblems to avoid redundant calculations.
+int DFS4(vector<int>& prices,int n,int i,vector<vector<int>>& dp){
+    if(i>=n || n<=0){
+        return 0;
+    }
+    if(dp[i][n]!=-1){
+        return dp[i][n];
+    }
+    int exclude=DFS4(prices,n,i+1,dp);
+    int include=0;
+    if(i+1<=n){
+        include=prices[i]+DFS4(prices,n-(i+1),i,dp);
+    }
+    dp[i][n]=max(include,exclude);
+    return dp[i][n];
+} 
+int rodCuttingBetter(vector<int>& prices){
+    int n=prices.size();
+    vector<vector<int>> dp(n,vector<int>(n+1,-1));
+    return DFS4(prices,n,0,dp);
+} // TC=O(N^2) SC=O(N^2)+O(N)
+//For Optimal Approach, we will use tabulation to build a DP table iteratively.
+int rodCuttingOptimal(vector<int>& prices){
+    int n=prices.size();
+    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            int include=0;
+            int exclude=dp[i-1][j];
+            if(i<=j){
+                include=prices[i-1]+dp[i][j-i];
+            }
+            dp[i][j]=max(include,exclude);
+        }
+    }
+    return dp[n][n];
+} // TC=O(N^2) SC=O(N^2)
+//For Space Optimized Approach, we will use a single array to store the maximum values for each rod length.
+int rodCuttingSpaceOptimized(vector<int>& prices){
+    int n=prices.size();
+    vector<int> dp(n+1,0);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<=n;j++){
+            dp[j]=max(dp[j],prices[i]+dp[j-(i+1)]);
+        }
+    }
+    return dp[n];
+} // TC=O(N^2) SC=O(N)
 int main(){
     int n=3,w=10;
     vector<int> weights={2,4,6};
@@ -74,5 +141,10 @@ int main(){
     cout<<knapsackBetter(weights,values,w)<<endl;
     cout<<knapsackOptimal(weights,values,w)<<endl;
     cout<<knapsackSpaceOptimized(weights,values,w)<<endl;
+    vector<int> prices={2,5,9,6};
+    cout<<rodCuttingBruteForce(prices)<<endl;
+    cout<<rodCuttingBetter(prices)<<endl;
+    cout<<rodCuttingOptimal(prices)<<endl;
+    cout<<rodCuttingSpaceOptimized(prices)<<endl;
     return 0;
 }
