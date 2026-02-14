3bc495d5485864847281bc1a7b8c35252ae715bd
Harsh <harshtyagi0008@icloud.com>
2026-02-14 19:07:35 +0530

    add DP part 14, problem 2

--- DP14.cpp
+++ DP14.cpp
@@ -47,10 +47,91 @@ int longestCommonSubstringSpaceOptimized(string text1,string text2){
     }
     return maxLength;
 } // TC=O(M*N) SC=O(N)
+//Problem 2: Longest Palindromic Subsequence
+//For Brute Force Approach, we can use recursion to find all possible subsequences of the string and check if they are palindromic to find the longest palindromic subsequence.
+int DFS(string s,int i,int j){
+    if(i>1){
+        return 0;
+    }
+    if(s[i]==s[j]){
+        if(i==j){
+            return 1;
+        }
+        return 2+DFS(s,i+1,j-1);
+    }
+    return max(DFS(s,i+1,j),DFS(s,i,j-1));
+}
+int longestPalindromeSubsequenceBruteForce(string s){
+    return DFS(s,0,s.size()-1);
+} // TC=O(2^N) SC=O(N)
+//For Better Approach, we can use memoization to store the results of previously computed subproblems and avoid redundant calculations.
+int DFS2(string s,int i,int j,vector<vector<int>>& dp){
+    if(i>1){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];    
+    }
+    if(s[i]==s[j]){
+        if(i==j){
+            dp[i][j]=1;
+        }
+        else{
+            dp[i][j]=2+DFS2(s,i+1,j-1,dp);
+        }
+    }
+    else{
+        dp[i][j]=max(DFS2(s,i+1,j,dp),DFS2(s,i,j-1,dp));
+    }
+    return dp[i][j];
+}
+int longestPalindromeSubsequenceBetter(string s){
+    vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
+    return DFS2(s,0,s.size()-1,dp);
+} // TC=O(N*N) SC=O(N*N)+O(N)
+//For Optimal Approach, we will use tabulation to fill a 2D array where dp[i][j] represents the length of the longest palindromic subsequence in the substring s[i...j].
+int longestPalindromeSubsequenceOptimal(string s){
+    vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
+    for(int i=s.size()-1;i>=0;i--){
+        dp[i][i]=1;
+        for(int j=i+1;j<s.size();j++){
+            if(s[i]==s[j]){
+                dp[i][j]=2+dp[i+1][j-1];    
+            }
+            else{
+                dp[i][j]=max(dp[i+1][j],dp[i][j-1]);
+            }   
+        }
+    }
+    return dp[0][s.size()-1];
+} // TC=O(N*N) SC=O(N*N)
+//For Space Optimized Approach, we will optimize by using two 1D arrays to store the results of the previous and current rows of the dp table, but we will need to keep track of the indices to reconstruct the longest palindromic subsequence.
+int longestPalindromeSubsequenceSpaceOptimized(string s){
+    vector<int> prev(s.size(),0),curr(s.size(),0);
+    for(int i=s.size()-1;i>=0;i--){
+        curr[i]=1;
+        for(int j=i+1;j<s.size();j++){
+            if(s[i]==s[j]){
+                curr[j]=2+prev[j-1];
+            }
+            else{
+                curr[j]=max(prev[j],curr[j-1]);
+            }
+        }
+        prev=curr;
+    }
+    return prev[s.size()-1];
+} // TC=O(N*N) SC=O(N)
+
 int main(){
     string s1="abcjklp",s2="acjkp";
     cout<<longestCommonSubstringBruteForce(s1,s2)<<endl; 
     cout<<longestCommonSubstringOptimal(s1,s2)<<endl;
     cout<<longestCommonSubstringSpaceOptimized(s1,s2)<<endl;
+    string s="bbbab";
+    cout<<longestPalindromeSubsequenceBruteForce(s)<<endl;
+    cout<<longestPalindromeSubsequenceBetter(s)<<endl;
+    cout<<longestPalindromeSubsequenceOptimal(s)<<endl;
+    cout<<longestPalindromeSubsequenceSpaceOptimized(s)<<endl;
     return 0;
 }
