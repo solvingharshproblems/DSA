25afb43a631ff33ed16a78d3b4920d0353152487
Harsh <harshtyagi0008@icloud.com>
2026-03-19 21:59:50 +0530

    add DP part 20, problem 3

--- DP20.cpp
+++ DP20.cpp
@@ -46,6 +46,44 @@ vector<int> largestDivisibleSubset(vector<int>& nums){
     reverse(result.begin(), result.end());
     return result;
 } // TC=O(n^2) SC=O(n)
+//Problem 3: Longest String Chain
+//A string chain is a sequence of words [word1, word2, ..., wordk] such that word1 is a predecessor of word2, word2 is a predecessor of word3 and so on. A word A is a predecessor of B if and only if we can add exactly one letter anywhere in A to get B.
+//For Optimal Approach, we will sort the given array of strings based on their lengths and then use a similar approach as the longest increasing subsequence to find the longest string chain.
+bool isPredecessor(string& a,string& b){
+    if(b.length()-a.length()!=1){
+        return false;
+    }
+    int i=0,j=0;
+    while(i<a.length() && j<b.length()){
+        if(a[i]==b[j]){
+            i++;
+        }
+        j++;
+    }
+    return i==a.length();
+}
+bool compare(string& a,string& b){
+    return a.length()<b.length();
+}
+int longestStrChain(vector<string>& words){
+    sort(words.begin(),words.end(),compare);
+    vector<int> dp(words.size(),1),hash(words.size());
+    int maxLen=1,lastIndex=0;
+    for(int i=0;i<words.size();i++){
+        hash[i]=i;
+        for(int j=0;j<i;j++){
+            if(isPredecessor(words[j],words[i]) && dp[i]<dp[j]+1){
+                dp[i]=dp[j]+1;
+                hash[i]=j;
+            }
+        }
+        if(dp[i]>maxLen){
+            maxLen=dp[i];
+            lastIndex=i;
+        }
+    }
+    return maxLen;
+} // TC=O(n^2*m) SC=O(n)
 int main(){
     int n=6;
     vector<int> nums={10,9,2,5,3,7,101,18};
@@ -55,5 +93,7 @@ int main(){
         cout<<result[i]<<" ";
     }
     cout<<endl;
+    vector<string> words={"a","b","ba","bca","bda","bdca"};
+    cout<<longestStrChain(words)<<endl;
     return 0;
 }
